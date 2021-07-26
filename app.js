20a346b89ace1b468d0551a8145868b308f62caf
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2021-07-26 21:07:23 +0300

    Startup on

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const startup = require('./startup');
 const app = express();
 const port = process.env.PORT || config.PORT;
 
-// startup(); //Start scanning groups on startup
+startup(); //Start scanning groups on startup
 
 app.use(cors());
 app.use(express.json());
