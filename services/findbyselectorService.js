712ed1b0ecb787ed01463ee2ddfcbe619f18187a
Mendi Bass <51884406+MMBass@users.noreply.github.com>
2021-07-27 22:00:13 +0300

    Div selector test

--- services/findbyselectorService.js
+++ services/findbyselectorService.js
@@ -63,7 +63,7 @@ async function getDom(group_id) {
       await page.waitForTimeout( generateRandSeconds());
    
       let divsText = await page.evaluate(() => {
-        const results = Array.from(document.querySelectorAll(`div[data-ad-preview="message"]`));
+        const results = Array.from(document.querySelectorAll(`div`));
         return results.map((div) => div.innerText);
       });
       await page.close();
