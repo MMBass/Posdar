ed60e0a94816c5a297bc1e92df2868a154a020b0
unknown <mendibass@gmail.com>
2021-08-01 12:26:32 +0300

    email subject removed

--- services/sendnewpostsService.js
+++ services/sendnewpostsService.js
@@ -4,14 +4,14 @@ const tasksModel = require('../models/tasks');
 
 exports.sendEmails = async function (newRelevant,task) {
   const rand = (Math.random() + 1).toString(36).substring(2);
-  const subject = "Posdar. New post in group " + task.group + "  (ID:"+ rand+ ")";
+  const subject = "Posdar New post in group " + task.group + "  (ID:"+ rand+ ")";
   const link = `Link to group: ` + config.fbLink(task.group);
   const email = task.email;
 
   for (postText of newRelevant) {
     try {
       await emailService.sendEmail({
-        subject,
+        postText,
         template: `<div style="width:95%; text-align:center; background-color:antiquewhite; margin: 5px;">
                       <h3 style="background-color:aliceblue; padding: 8px;">${subject}</h3>
                       <strong>${postText}</strong>
