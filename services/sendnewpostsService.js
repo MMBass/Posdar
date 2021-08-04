b7e87821f656bc2cc83b67839215fe98749c4c4e
unknown <mendibass@gmail.com>
2021-08-04 14:55:18 +0300

    rand id for each email

--- services/sendnewpostsService.js
+++ services/sendnewpostsService.js
@@ -3,16 +3,16 @@ const config = require('../config/config');
 const tasksModel = require('../models/tasks');
 
 exports.sendEmails = async function (newRelevant,task) {
-  const rand = (Math.random() + 1).toString(36).substring(2);
-  const subject = "Posdar New post in group " + task.group + "  (ID:"+ rand+ ")";
   const link = `Link to group: ` + config.fbLink(task.group);
   const email = task.email;
 
   for (postText of newRelevant) {
+    const rand = (Math.random() + 1).toString(36).substring(2);
+    const subject = "Posdar New post in group " + task.group + "  (ID:"+ rand+ ")";
     try {
       await emailService.sendEmail({
         postText,
-        template: `<div style="width:95%; text-align:center; background-color:antiquewhite; margin: 5px;">
+        template: `<div style="width:100%; text-align:center; background-color:antiquewhite; margin: 5px;">
                       <h3 style="background-color:aliceblue; padding: 8px;">${subject}</h3>
                       <strong>${postText}</strong>
                       <p style="background-color: lightgray; padding: 8px;">${link}</p>
