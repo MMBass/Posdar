8acd18d8d00a4e14019034803ea1c962961bf5ed
unknown <mendibass@gmail.com>
2021-08-09 05:19:48 +0300

    get all route

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -44,6 +44,30 @@ exports.newRegister = [
     }
 ];
 
+exports.getRegisters = [
+    // Validate and santitize fields.
+    body('userName', "userName doesn't exists").exists().isEmail().escape(),
+
+    // Process request after validation and sanitization.
+    async (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+        const task = req.body;
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+            res.status(200).send({ message: "Some of the fields missing or incorrect" });
+            return;
+        }
+        else {
+            const all = await tasksModel.readAll();
+            // Data from form is valid. Delete the task.
+            res.send({tasks: all});
+        }
+    }
+];
+
 exports.delRegister = [
     // Validate and santitize fields.
     body('userName', "userName doesn't exists").exists().isEmail().escape(),
--- routes/register.js
+++ routes/register.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const registerController  = require('../controllers/registerController');
 
-/* POST json object. */
+router.get('/',registerController.getRegisters);
+
 router.post('/',registerController.newRegister);
 
-/* DELETE json object. */
 router.delete('/', registerController.delRegister);
 
 module.exports = router;
